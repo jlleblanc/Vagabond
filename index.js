7c8b037ae5b9209c82d5e48a9d1f155c386eb358
Joe LeBlanc <contact@jlleblanc.com>
2012-06-22 22:29:34 -0500

    Switched to Handlebars for templating

--- index.js
+++ index.js
@@ -2,21 +2,28 @@ var db = require('./database').db;
 var express = require('express');
 
 var app = express.createServer();
+app.set('view engine', 'hbs');
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/admin', function  (req, res) {
+  res.render('admin.ejs', {title: 'Admin'});
+});
 
 app.get('/*', function(req, res){
   
   db.collection('pages', {safe: true}, function  (err, collection) {
     if (err) {
-      res.render('layout.ejs', {title: 'ERROR', body: 'error: ' + err});
+      res.render('layout.hbs', {title: 'ERROR', body: 'error: ' + err});
     } else {
       collection.findOne({'url': req.url}, function  (err, item) {
         if (err) {
-          res.render('layout.ejs', {title: 'ERROR', body: 'error: ' + err});
+          res.render('layout.hbs', {title: 'ERROR', body: 'error: ' + err});
         } else {
           if (item) {
-            res.render('layout.ejs', {title: item.title, body: item.body});
+            res.render('layout.hbs', {title: item.title, body: item.body});
           } else {
-            res.render('layout.ejs', {title: 'Not Found', body: 'item not found'});
+            res.render('layout.hbs', {title: 'Not Found', body: 'item not found'});
           }
         }
       });
