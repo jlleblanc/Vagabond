baa509ea4e39e9245bcd857a4c8642c5170f41d0
Joe LeBlanc <contact@jlleblanc.com>
2012-09-22 19:56:05 -0500

    Code cleanup

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
-var db = require('./database');
-var config = require('./config');
-var server = require('./server');
+var db = require('./database'),
+  config = require('./config'),
+  server = require('./server');
 
 server.get('/*', function(req, res){
   
-  db.Page.find({url: req.url}, function  (err, page) {
+  db.Page.findOne({url: req.url}, function  (err, page) {
     if (err) {
       res.render('layout.hbs', {title: 'ERROR', body: 'error: ' + err});
     } else {
-      if (page.length) {
-        page = page[0];
-        res.render('layout.hbs', {title: page.title, body: page.body});        
+      if (page) {
+        res.render('layout.hbs', {title: page.title, content: page.body, user: req.user});        
       } else {
         res.status(404).render('layout.hbs', {title: 'ERROR', body: 'Not Found'});
       }
