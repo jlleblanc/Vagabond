2585e3999fe964752151409a5e5816c94099044a
Joe LeBlanc <contact@jlleblanc.com>
2012-09-22 20:11:37 -0500

    Now the frontend looks a little less ugly.

--- index.js
+++ index.js
@@ -3,15 +3,17 @@ var db = require('./database'),
   server = require('./server');
 
 server.get('/*', function(req, res){
-  
+
+  var styles = ['/media/bootstrap/css/bootstrap.min.css'];
+
   db.Page.findOne({url: req.url}, function  (err, page) {
     if (err) {
-      res.render('layout.hbs', {title: 'ERROR', body: 'error: ' + err});
+      res.render('layout.hbs', {title: 'ERROR', body: 'error: ' + err, styles: styles});
     } else {
       if (page) {
-        res.render('layout.hbs', {title: page.title, content: page.body, user: req.user});        
+        res.render('layout.hbs', {title: page.title, content: page.body, user: req.user, styles: styles});        
       } else {
-        res.status(404).render('layout.hbs', {title: 'ERROR', body: 'Not Found'});
+        res.status(404).render('layout.hbs', {title: 'ERROR', body: 'Not Found', styles: styles});
       }
     }
   });
