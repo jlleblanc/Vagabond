35c13ca1005c480cbad374b78bfac34cff9d23ae
Joe LeBlanc <contact@jlleblanc.com>
2012-10-06 18:10:21 -0500

    Cleaned up the database layer

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 // TODO: either put all of this in a try/catch or find a better way of 
 // logging errors/create a debug mode
 
-var db = require('./lib/database'),
-  server = require('./lib/server'),
+var server = require('./lib/server'),
   conf = require('./lib/config'),
   router = require('./lib/router');
-  
+
 router.addRoutes(server);
 server.listen(conf.get('port')); 
 
