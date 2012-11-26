2831d4aef04bda81a7070e80e455cda41609b4bc
Joe LeBlanc <contact@jlleblanc.com>
2012-11-25 21:07:16 -0600

    Added a REPL for the server and app objects.

--- index.js
+++ index.js
@@ -10,4 +10,11 @@ app.init();
 router(server, app);
 server.listen(app.config.get('port')); 
 
+if (app.config.get('repl') === true) {
+  repl = require('repl');
+  var prompt = repl.start({prompt: "vagabond> "});
+  prompt.context.app = app;
+  prompt.context.server = server;
+}
+
 module.exports = server;
