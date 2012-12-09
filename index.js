9cc2bd229b65e4614b984b40f9d6bd03164d780f
Joe LeBlanc <contact@jlleblanc.com>
2012-12-08 20:59:47 -0600

    Removed the REPL for now

--- index.js
+++ index.js
@@ -10,11 +10,4 @@ app.init();
 router(server, app);
 server.listen(app.config.get('port')); 
 
-if (app.config.get('repl') === true) {
-  repl = require('repl');
-  var prompt = repl.start({prompt: "vagabond> "});
-  prompt.context.app = app;
-  prompt.context.server = server;
-}
-
 module.exports = server;
